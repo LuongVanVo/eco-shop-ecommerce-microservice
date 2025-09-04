56495db82aac667f37aa79a9bb68c4e504cd6e6a
vanvo299 <luongvanvo29@gmail.com>
2025-09-04 16:53:58 +0700

    style: add error handler to replace try/catch

--- services/user-service/src/routes/user.routes.ts
+++ services/user-service/src/routes/user.routes.ts
@@ -3,11 +3,15 @@ import express from 'express'
 import { check, checkSchema } from "express-validator";
 import { validateSignup, validateLogin } from "../middleware/validationInput";
 import { validateRefreshTokenMiddleware } from "../utils/authToken";
+import { asyncHandler } from '../helpers/asyncHandler'
+import { errorHandler } from "../middleware/errorHandler";
 
 const router = express.Router()
 
-router.post('/signup', checkSchema(validateSignup), UserController.register)
-router.post('/login', checkSchema(validateLogin), UserController.login)
-router.post('/logout', validateRefreshTokenMiddleware, UserController.logout)
+router.post('/signup', checkSchema(validateSignup), asyncHandler(UserController.register))
+router.post('/login', checkSchema(validateLogin), asyncHandler(UserController.login))
+router.post('/logout', validateRefreshTokenMiddleware, asyncHandler(UserController.logout))
+
+router.use(errorHandler)
 
 export default router
