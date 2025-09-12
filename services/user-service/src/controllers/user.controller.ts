f9577006d7fc0d657676f2c70528e08c5110d3d4
vanvo299 <luongvanvo29@gmail.com>
2025-09-12 23:35:53 +0700

    feat/add feature update profile: cloudinary

--- services/user-service/src/controllers/user.controller.ts
+++ services/user-service/src/controllers/user.controller.ts
@@ -84,6 +84,23 @@ class UserController {
             metadata: data
         }).send(res);
     }
+
+    updateProfile = async (req: Request, res: Response, next: any) => {
+        // userId lấy từ middleware xác thực
+        const userId = req.user?.id!
+        const token = req.token
+
+        const profileData = {
+            userId: userId,
+            ...req.body
+        }
+
+        const data = await UserService.uploadProfile(profileData)
+        return new SuccessResponse({
+            message: "Profile updated successfully !!",
+            metadata: data
+        }).send(res)
+    }
 }
 
 export default new UserController();
